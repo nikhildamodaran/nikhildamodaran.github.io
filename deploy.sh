e73aedd6dcb8fa012bcffcc749de7b52ebb6669e
Nikhil Damodaran <nikhil.damodaran@gmail.com>
2026-04-23 23:08:50 +1000

    changed subheading

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+echo "What did you change? (commit message):"
+read msg
+
 quarto render .
 git add -A
-git commit -m "update site"
+git commit -m "$msg"
 git push
